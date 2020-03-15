dd251c47678806d585365056302bdd4bde88830b
yuta-666 <60540644+yuta-666@users.noreply.github.com>
2020-03-15 11:18:25 +0900

    create-auto-update

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,7 +1,7 @@
 $(function() {
   function buildHTML(message){
-    if ( message.image ) {
-      var html = `<div class="message">
+    var image = ( message.image ) ? `<img class="lower-message__image" src=${message.image} >` : "";
+      var html = `<div class="message", data-message-id="${message.id}">
                     <div class="message__info">
                       <p class="message__info__talker">
                         ${message.user_name}
@@ -14,29 +14,12 @@ $(function() {
                       <p class="lower-message__content">
                         ${message.content}
                       </p>
+                        ${image}
                     </div>
-                      <img class="lower-message__image" src=${message.image} >
                   </div>`
                 return html;
-    } else {
-      var html =`<div class="message">
-                  <div class="message__info">
-                    <p class="message__info__talker">
-                      ${message.user_name}
-                    </p>
-                    <p class="message__info__data">
-                      ${message.created_at}
-                    </p>
-                  </div>
-                  <div class="lower-message">
-                    <p class="lower-message__content">
-                      ${message.content}
-                    </p>
-                  </div>
-                </div>`
-              return html;
             };
-          }
+
   $('#new_message').on('submit', function(e) {
     e.preventDefault();
     var formData = new FormData(this);
@@ -58,5 +41,32 @@ $(function() {
       .fail(function() {
         alert("メッセージ送信に失敗しました");
       });
-    })
-  });
+    });
+
+    var reloadMessages = function() {
+      var last_message_id = $('.message:last').data("message-id");
+      $.ajax({
+        url: 'api/messages',
+        type: 'get',
+        dataType: 'json',
+        data: {id: last_message_id}
+      })
+      .done(function(messages) {
+        if (messages.length !== 0) {
+          var insertHTML = '';
+          $.each(messages, function(i, message) {
+            insertHTML += buildHTML(message)
+          });
+          $('.messages').append(insertHTML);
+          $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+        }
+      })
+      .fail(function() {
+        alert('error');
+      });
+    };
+
+    if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+      setInterval(reloadMessages, 7000);
+    }
+});
